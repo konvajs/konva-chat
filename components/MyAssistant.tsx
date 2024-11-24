6eced8e34e7a0211f5ad25758d592081661041c0
Anton Lavrevov <lavrton@gmail.com>
2024-11-24 15:07:10 -0500

    clean

--- components/MyAssistant.tsx
+++ components/MyAssistant.tsx
@@ -3,17 +3,6 @@
 import { useEdgeRuntime } from "@assistant-ui/react";
 import { Thread } from "@assistant-ui/react";
 import { makeMarkdownText } from "@assistant-ui/react-markdown";
-import { ThreadPrimitive } from "@assistant-ui/react";
-
-const Suggestion = () => {
-  return (
-    <ThreadPrimitive.Suggestion
-      prompt="I need help with product search"
-      method="replace"
-      autoSend
-    />
-  );
-};
 
 const MarkdownText = makeMarkdownText();
 
@@ -25,6 +14,8 @@ export function MyAssistant() {
       runtime={runtime}
       assistantMessage={{ components: { Text: MarkdownText } }}
       welcome={{
+        message:
+          "Hello! I'm Kai, your konva assistant. How can I help you today?",
         suggestions: [
           { prompt: "How do I create a draggable rectangle with Konva?" },
           { prompt: "How to add image with react-konva?" },
